983914312ad764176dec162416b83f13fb254035
cj <cjlazell@gmail.com>
2015-03-30 18:22:03 -0500

    added forms and history plugins

--- lib/browserio/plugins/form.rb
+++ lib/browserio/plugins/form.rb
@@ -1,5 +1,343 @@
+require 'browserio/plugins/validations'
+require 'forwardable'
+
 module BrowserIO
-  class Form < Component
-    config.name :form_plugin
+  module Plugins
+    class Form < Component
+      config.name :form_plugin
+
+      include Methods
+      include Validations
+
+      module Delegates
+        def _delegates(*names)
+          accessors = Module.new do
+            extend Forwardable # DISCUSS: do we really need Forwardable here?
+            names.each do |name|
+              delegate [name, "#{name}="] => :_attributes
+            end
+          end
+          include accessors
+        end
+      end
+
+      extend Delegates
+
+      class Attributes
+        def set_values(atts)
+          @_attributes = []
+
+          atts.each do |key, val|
+            if respond_to?("#{key}=")
+              send(:"#{key}=", val)
+              @_attributes << key
+            end
+          end
+        end
+
+        def set_attr_accessors attrs
+          attrs.each do |attr|
+            define_singleton_method "#{attr}=" do |value|
+              value = value.to_obj if value.is_a? Hash
+              instance_variable_set(:"@#{attr}", value)
+              @_attributes ||= []
+              @_attributes << attr
+            end
+
+            define_singleton_method attr do
+              instance_variable_get(:"@#{attr}")
+            end
+          end
+        end
+
+        def _attributes
+          @_attributes ||= []
+        end
+
+        def empty?
+          _attributes.empty?
+        end
+      end
+
+      # Initialize with a hash of attributes and values.
+      # If extra attributes are sent, a NoMethodError exception will be raised.
+      #
+      # @example
+      #
+      #   class EditPost < Scrivener
+      #     attr_accessor :title
+      #     attr_accessor :body
+      #
+      #     def validate
+      #       assert_present :title
+      #       assert_present :body
+      #     end
+      #   end
+      #
+      #   edit = EditPost.new(title: "Software Tools")
+      #
+      #   edit.valid? #=> false
+      #
+      #   edit.errors[:title] #=> []
+      #   edit.errors[:body]  #=> [:not_present]
+      #
+      #   edit.body = "Recommended reading..."
+      #
+      #   edit.valid? #=> true
+      #
+      #   # Now it's safe to initialize the model.
+      #   post = Post.new(edit.attributes)
+      #   post.save
+      def initialize(atts = {}, options = {})
+        @_data    = atts
+        @_data    = atts.to_obj if atts.is_a? Hash
+        @_options = options
+
+        # @_attributes = Class.new(Attributes).new
+        @_attributes = Attributes.new
+        @_attributes.set_attr_accessors _attr_accessors
+        @_attributes.set_values _data
+
+        _data.each do |key, val|
+          send("#{key}=", val)
+        end
+
+        _form.each do |key, klass|
+          opts = {}
+          opts[key] = klass.new(_data.send(key)) if _data.respond_to?(key)
+          @_attributes.set_values opts
+
+          send("#{key}=", opts[key])
+        end
+      end
+
+      def self.attr_accessor(*vars)
+        @_attr_accessors ||= []
+        @_form ||= {}
+
+        vars.each do |v|
+          if !v.is_a? Hash
+            @_attr_accessors << v unless @_attr_accessors.include? v
+          else
+            v = v.first
+
+            unless @_attr_accessors.include? v.first
+              @_attr_accessors << v.first
+              @_form[v.first] = v.last
+            end
+          end
+        end
+
+        _delegates(*_attr_accessors)
+      end
+
+      def method_missing method, *args, &block
+        # respond_to?(symbol, include_all=false)
+        if _data.respond_to? method, true
+          _data.send method, *args, &block
+        else
+          return if method[/\=\z/]
+
+          super
+        end
+      end
+
+      # Return hash of attributes and values.
+      def attributes
+        Hash.new.tap do |atts|
+          _attributes.instance_variables.each do |ivar|
+            # todo: figure out why it's setting @constructor and @toString
+            next if ivar == :@constructor || ivar == :@toString || ivar == :@_attributes || ivar == :@_data || ivar == :@_forms
+
+            att = ivar[1..-1].to_sym
+            atts[att] = _attributes.send(att)
+
+            if klass = _form[att.to_s.to_sym]
+              atts[att] = klass.new(atts[att]).attributes
+            end
+          end
+        end
+      end
+
+      def model_attributes data = attributes
+        hash = {}
+
+        data.each do |k, v|
+          if klass = _form[k.to_s.to_sym]
+            d = data[k]
+            d = d.attributes if d.is_a?(Form)
+
+            f  = klass.new d
+            k  = "#{k}_attributes"
+            dt = f.model_attributes
+
+            hash[k] = model_attributes dt
+          elsif v.is_a? Hash
+            hash[k] = model_attributes data[k]
+          else
+            hash[k] = v
+          end
+        end
+
+        hash
+      end
+
+      def slice(*keys)
+        Hash.new.tap do |atts|
+          keys.each do |att|
+            atts[att] = send(att)
+            # atts[att] = _attributes.send(att)
+          end
+        end
+      end
+
+      def display_errors options = {}, &block
+        dom = options.delete(:dom) || _dom
+        d_errors = errors
+
+        if override_errors = options[:override_errors]
+          d_errors = override_errors
+        end
+
+        keys = options.delete(:keys) || (_options[:key] ? [_options[:key]] : [])
+
+        if extra_errors = options.delete(:errors)
+          extra_errors.each do |key, value|
+            d_errors[key] = value
+          end
+        end
+
+        d_errors.each do |key, error|
+          d_keys = (keys.dup << key)
+
+          error = error.first
+
+          if error.is_a?(Hash)
+            d_options = options.dup
+            d_options[:keys] = d_keys
+            d_options[:override_errors] = d_errors[key].first
+
+            display_errors d_options, &block
+          elsif !block_given? || block.call(d_keys, error) == false
+            name = d_keys.each_with_index.map do |field, i|
+              i != 0 ? "[#{field}]" : field
+            end.join
+
+            if tmpl = options[:tmpl]
+              if client?
+                field_error_dom = DOM.new(`#{tmpl.dom}[0].outerHTML`)
+              else
+                field_error_dom = DOM.new(tmpl.dom.to_html)
+              end
+            else
+              field_error_dom = DOM.new('<span class="field-error"><span>')
+            end
+
+            field_error_dom.html _error_name(key, error)
+
+            field = dom.find("[name='#{name}']")
+            field.before field_error_dom.dom
+          end
+        end
+      end
+      alias_method :render_errors, :display_errors
+
+      def render_values dom = false, key = false, data = false
+        dom = _options[:dom] unless dom
+        key = _options[:key] if !key && _options.key?(:key)
+
+        dom.find('input, select, textarea') do |element|
+          name  = element['name']
+          name  = name.gsub(/\A#{key}/, '') if key
+          keys  = name.gsub(/\A\[/, '').gsub(/[^a-z0-9_]/, '|').gsub(/\|\|/, '|').gsub(/\|$/, '').split('|')
+          value = false
+
+          keys.each do |k|
+            begin
+              value = value ? value.send(k) : send(k)
+            rescue
+              value = ''
+            end
+          end
+
+          case element.name
+          when 'select'
+            element.find('option') do |x|
+              x['selected'] = true if x['value'] == value.to_s
+            end
+          when 'input'
+            if %w(radio checkbox).include? element['type']
+              if element['value'] == value.to_s
+                element['checked'] = true
+              else
+                element.delete 'checked'
+              end
+            else
+              value = sprintf('%.2f', value) if value.is_a? BigDecimal
+              element['value'] = value.to_s
+            end
+          when 'textarea'
+            element.val value.to_s
+          end
+        end
+      end
+
+      def _attributes
+        @_attributes ||= {}
+      end
+
+      def validate_msg error, column
+        false
+      end
+
+      protected
+
+      def _data
+        @_data ||= {}
+      end
+
+      def self._attr_accessors
+        @_attr_accessors ||= []
+      end
+
+      def self._form
+        @_form || {}
+      end
+
+      def _form
+        self.class._form
+      end
+
+      def _attr_accessors
+        self.class._attr_accessors
+      end
+
+      def _options
+        @_options
+      end
+
+      def _dom
+        @_dom ||= @_options[:dom]
+      end
+
+      def _error_name key, error
+        validate_msg(error.to_sym, key.to_sym) || case error.to_s.to_sym
+        when :not_email
+          'Email Isn\'t Valid.'
+        when :not_present
+          'Required.'
+        when :not_equal
+          'Password does not match.'
+        else
+          !error[/\s/] ? error.to_s.gsub(/_/, ' ').titleize : error
+        end
+      end
+
+      def empty?
+        _attributes.empty?
+      end
+    end
   end
 end
+
+BrowserIO::Form = BrowserIO::Plugins::Form
---
+++ lib/browserio/plugins/validations.rb
@@ -0,0 +1,251 @@
+module BrowserIO
+  module Plugins
+    class Form < Component
+      # Provides a base implementation for extensible validation routines.
+      # {Scrivener::Validations} currently only provides the following assertions:
+      #
+      # * assert
+      # * assert_present
+      # * assert_format
+      # * assert_numeric
+      # * assert_url
+      # * assert_email
+      # * assert_member
+      # * assert_length
+      # * assert_decimal
+      # * assert_equal
+      #
+      # The core tenets that Scrivener::Validations advocates can be summed up in a
+      # few bullet points:
+      #
+      # 1. Validations are much simpler and better done using composition rather
+      #    than macros.
+      # 2. Error messages should be kept separate and possibly in the view or
+      #    presenter layer.
+      # 3. It should be easy to write your own validation routine.
+      #
+      # Other validations are simply added on a per-model or per-project basis.
+      #
+      # @example
+      #
+      #   class Quote
+      #     attr_accessor :title
+      #     attr_accessor :price
+      #     attr_accessor :date
+      #
+      #     def validate
+      #       assert_present :title
+      #       assert_numeric :price
+      #       assert_format  :date, /\A[\d]{4}-[\d]{1,2}-[\d]{1,2}\z
+      #     end
+      #   end
+      #
+      #   s = Quote.new
+      #   s.valid?
+      #   # => false
+      #
+      #   s.errors
+      #   # => { :title => [:not_present],
+      #          :price => [:not_numeric],
+      #          :date  => [:format] }
+      #
+      module Validations
+        def server? &block
+          RUBY_ENGINE == 'ruby'
+        end
+        alias :server :server?
+
+        def client?
+          RUBY_ENGINE == 'opal'
+        end
+        alias :client :client?
+
+        def self.server? &block
+          RUBY_ENGINE == 'ruby'
+        end
+        alias :server :server?
+
+        def self.client?
+          RUBY_ENGINE == 'opal'
+        end
+        alias :client :client?
+
+        # Check if the current model state is valid. Each call to {#valid?} will
+        # reset the {#errors} array.
+        #
+        # All validations should be declared in a `validate` method.
+        #
+        # @example
+        #
+        #   class Login
+        #     attr_accessor :username
+        #     attr_accessor :password
+        #
+        #     def validate
+        #       assert_present :user
+        #       assert_present :password
+        #     end
+        #   end
+        #
+        def valid?
+          errors.clear
+          validate
+          errors.empty?
+        end
+
+        # Base validate implementation. Override this method in subclasses.
+        def validate
+        end
+
+        # Hash of errors for each attribute in this model.
+        def errors
+          @errors ||= Hash.new { |hash, key| hash[key] = [] }
+        end
+
+        protected
+
+        # Allows you to do a validation check against a regular expression.
+        # It's important to note that this internally calls {#assert_present},
+        # therefore you need not structure your regular expression to check
+        # for a non-empty value.
+        #
+        # @param [Symbol] att The attribute you want to verify the format of.
+        # @param [Regexp] format The regular expression with which to compare
+        #                 the value of att with.
+        # @param [Array<Symbol, Symbol>] error The error that should be returned
+        #                                when the validation fails.
+        def assert_format(att, format, error = [att, :format])
+          if assert_present(att, error)
+            assert(_attributes.send(att).to_s.match(format), error)
+          end
+        end
+
+        # The most basic and highly useful assertion. Simply checks if the
+        # value of the attribute is empty.
+        #
+        # @param [Symbol] att The attribute you wish to verify the presence of.
+        # @param [Array<Symbol, Symbol>] error The error that should be returned
+        #                                when the validation fails.
+        def assert_present(att, error = [att, :not_present])
+          if att.is_a? Array
+            att.each { |a| assert_present(a, error = [a, :not_present])}
+          else
+            if klass = _form[att]
+              options = {}
+              options[:key] = _options[:key] if _options.key? :key
+
+              f = klass.new(_attributes.send(att).attributes, options)
+              assert(f.valid?, [att, f.errors])
+            else
+              assert(!_attributes.send(att).to_s.empty?, error)
+            end
+          end
+        end
+
+        # Checks if all the characters of an attribute is a digit.
+        #
+        # @param [Symbol] att The attribute you wish to verify the numeric format.
+        # @param [Array<Symbol, Symbol>] error The error that should be returned
+        #                                when the validation fails.
+        def assert_numeric(att, error = [att, :not_numeric])
+          if assert_present(att, error)
+            if client?
+              assert_format(att, /^\-?\d+$/, error)
+            else
+              assert_format(att, /\A\-?\d+\z/, error)
+            end
+          end
+        end
+
+        if client?
+          URL = /^(http|https):\/\/([a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}|(2 5[0-5]|2[0-4]\d|[0-1]?\d?\d)(\.(25[0-5]|2[0-4]\d|[0-1]?\d?\d)){3} |localhost)(:[0-9]{1,5})?(\/.*)?$/i
+        else
+          URL = /\A(http|https):\/\/([a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}|(2 5[0-5]|2[0-4]\d|[0-1]?\d?\d)(\.(25[0-5]|2[0-4]\d|[0-1]?\d?\d)){3} |localhost)(:[0-9]{1,5})?(\/.*)?\z/i
+        end
+
+        def assert_url(att, error = [att, :not_url])
+          if assert_present(att, error)
+            assert_format(att, URL, error)
+          end
+        end
+
+        if client?
+          EMAIL = /^[a-z0-9!\#$%&'*\/=\?^{|}+_-]+(?:\.[a-z0-9!\#$%&'*\/=\?^{|}+_-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/i
+        else
+          EMAIL = /\A[a-z0-9!\#$%&'*\/=\?^{|}+_-]+(?:\.[a-z0-9!\#$%&'*\/=\?^{|}+_-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\z/i
+        end
+
+        def assert_email(att, error = [att, :not_email])
+          if assert_present(att, error)
+            assert_format(att, EMAIL, error)
+          end
+        end
+
+        def assert_member(att, set, err = [att, :not_valid])
+          assert(set.include?(_attributes.send(att)), err)
+        end
+
+        def assert_length(att, range, error = [att, :not_in_range])
+          if assert_present(att, error)
+            val = _attributes.send(att).to_s
+            assert range.include?(val.length), error
+          end
+        end
+
+        if client?
+          DECIMAL = /^\-?(\d+)?(\.\d+)?$/
+        else
+          DECIMAL = /\A\-?(\d+)?(\.\d+)?\z/
+        end
+
+        def assert_decimal(att, error = [att, :not_decimal])
+          assert_format att, DECIMAL, error
+        end
+
+        # Check that the attribute has the expected value. It uses === for
+        # comparison, so type checks are possible too. Note that in order
+        # to make the case equality work, the check inverts the order of
+        # the arguments: `assert_equal :foo, Bar` is translated to the
+        # expression `Bar === send(:foo)`.
+        #
+        # @example
+        #
+        #   def validate
+        #     assert_equal :status, "pending"
+        #     assert_equal :quantity, Fixnum
+        #   end
+        #
+        # @param [Symbol] att The attribute you wish to verify for equality.
+        # @param [Object] value The value you want to test against.
+        # @param [Array<Symbol, Symbol>] error The error that should be returned
+        #                                when the validation fails.
+        def assert_equal(att, value, error = [att, :not_equal])
+          assert value === _attributes.send(att), error
+        end
+
+        # The grand daddy of all assertions. If you want to build custom
+        # assertions, or even quick and dirty ones, you can simply use this method.
+        #
+        # @example
+        #
+        #   class CreatePost
+        #     attr_accessor :slug
+        #     attr_accessor :votes
+        #
+        #     def validate
+        #       assert_slug :slug
+        #       assert votes.to_i > 0, [:votes, :not_valid]
+        #     end
+        #
+        #   protected
+        #     def assert_slug(att, error = [att, :not_slug])
+        #       assert send(att).to_s =~ /\A[a-z\-0-9]+\z/, error
+        #     end
+        #   end
+        def assert(value, error)
+          value or errors[error.first].push(error.last) && false
+        end
+      end
+    end
+  end
+end
