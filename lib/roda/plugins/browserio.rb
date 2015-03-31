da80f4d9a7fae91d4c8915b0230b6c77f727a000
cj <cjlazell@gmail.com>
2015-03-30 19:12:35 -0500

    added roda plugin

---
+++ lib/roda/plugins/browserio.rb
@@ -0,0 +1,64 @@
+class Roda
+  module RodaPlugins
+    module BrowserIO
+      def self.configure(app, opts = {})
+        if app.opts[:browserio]
+          app.opts[:browserio].merge!(opts)
+        else
+          app.opts[:browserio] = opts.dup
+        end
+
+        opts = app.opts[:browserio]
+
+
+        opts.each do |k, v|
+          case k.to_s
+          when 'plugins'
+            v.each { |p| ::BrowserIO.config.plugin p }
+          when 'scope'
+            ::BrowserIO.config.scope v.new('')
+          else
+            ::BrowserIO.config.send(k, v)
+          end
+        end
+      end
+
+      module InstanceMethods
+        def bio(*args)
+          args << { scope: self }
+          ::BrowserIO[*args]
+        end
+        alias_method :comp, :bio unless defined? comp
+        alias_method :component, :bio unless defined? component
+      end
+
+      module RequestClassMethods
+        def bio_route_regex
+          %r{assets/bio/(.*)\.(.*)$}
+        end
+      end
+
+      module RequestMethods
+        def browserio
+          on self.class.bio_route_regex do |component, ext|
+            case ext
+            when 'map'
+              ::BrowserIO.source_map component
+            when 'rb'
+              if component =~ /^browserio/
+                path = ::BrowserIO.opts.file_path.gsub(/\/browserio.rb$/, '')
+                File.read("#{path}/#{component}.rb")
+              else
+                File.read("#{ROOT_PATH}/#{component}.rb")
+              end
+            else
+              "#{::BrowserIO.javascript(component)}\n//# sourceMappingURL=/assets/bio/#{component}.map"
+            end
+          end
+        end
+      end
+    end
+
+    register_plugin(:browserio, BrowserIO)
+  end
+end
--- test/dummy/app.rb
+++ test/dummy/app.rb
@@ -7,17 +7,6 @@ require 'awesome_print'
 ROOT_PATH = File.dirname(__FILE__)
 
 class DummyApp < Roda
-  BrowserIO.setup do |c|
-    c.scope self.new('')
-    c.assets_url '/assets/bio'
-    c.plugin :form
-  end
-
-  def bio(*args)
-    args << { scope: self }
-    BrowserIO[*args]
-  end
-
   plugin :assets, {
     group_subdirs: false,
     path: ROOT_PATH,
@@ -26,23 +15,30 @@ class DummyApp < Roda
     js: [ 'bio/browserio.js' ]
   }
 
+  plugin :browserio, {
+    scope: self,
+    assets_url: '/assets/bio',
+    plugins: [:form]
+  }
+
   route do |r|
-    r.on %r{assets/bio/(.*)\.map} do |map|
-      BrowserIO.source_map map
-    end
-
-    r.on %r{assets/bio/(.*)\.rb} do |component|
-      if component =~ /^browserio/
-        path = BrowserIO.opts.file_path.gsub(/\/browserio.rb$/, '')
-        File.read("#{path}/#{component}.rb")
-      else
-        File.read("#{ROOT_PATH}/#{component}.rb")
-      end
-    end
-
-    r.on %r{assets/bio/(.*)\.js} do |component|
-      "#{BrowserIO.javascript(component)}\n//# sourceMappingURL=/assets/bio/#{component}.map"
-    end
+    r.browserio
+    # r.on %r{assets/bio/(.*)\.map} do |map|
+    #   BrowserIO.source_map map
+    # end
+    #
+    # r.on %r{assets/bio/(.*)\.rb} do |component|
+    #   if component =~ /^browserio/
+    #     path = BrowserIO.opts.file_path.gsub(/\/browserio.rb$/, '')
+    #     File.read("#{path}/#{component}.rb")
+    #   else
+    #     File.read("#{ROOT_PATH}/#{component}.rb")
+    #   end
+    # end
+    #
+    # r.on %r{assets/bio/(.*)\.js} do |component|
+    #   "#{BrowserIO.javascript(component)}\n//# sourceMappingURL=/assets/bio/#{component}.map"
+    # end
 
     r.root do
       bio(:root, :js).display
