e59dba1405bc8a861420dbd8cf4319ff0ccffeeb
cj <cjlazell@gmail.com>
2015-08-03 18:35:04 -0500

    update

--- lib/wedge.rb
+++ lib/wedge.rb
@@ -267,7 +267,7 @@ class Wedge
       o = [('a'..'z'), ('A'..'Z')].map { |i| i.to_a }.flatten
       key = (0...50).map { o[rand(o.length)] }.join
       ::FileUtils.mkdir_p(File.dirname('.wedge_assets_key'))
-      ::File.open("#{File.dir}/.wedge_assets_key", 'wb'){|f| f.write(key) }
+      ::File.open('.wedge_assets_key', 'wb'){|f| f.write(key) }
     end
 
     def config
@@ -276,7 +276,7 @@ class Wedge
 
         unless RUBY_ENGINE == 'opal'
           args[:path]       = method(:assets_url).source_location.first.sub('/wedge.rb', '')
-          args[:assets_key] = ::File.exist?("#{File.dir}/.wedge_assets_key") ? ::File.read("#{File.dir}/.wedge_assets_key") : false
+          args[:assets_key] = ::File.exist?('.wedge_assets_key') ? ::File.read('.wedge_assets_key') : false
         end
 
         Config.new(args)
