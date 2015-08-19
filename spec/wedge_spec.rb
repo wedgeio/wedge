b3af8ab9f8e5ade5dc3a51a985af5eb7f60f035d
cj <cjlazell@gmail.com>
2015-08-19 09:07:19 -0500

    updated tests to reflect assets key change

--- spec/wedge_spec.rb
+++ spec/wedge_spec.rb
@@ -2,18 +2,14 @@ require 'spec_helper'
 
 describe Wedge do
   context 'config' do
-    if Wedge.server?
-      it 'should have an assets key' do
-        expect(Wedge.config.assets_key).not_to be_nil
-      end
+    it 'should not have an assets key' do
+      expect(Wedge.config.assets_key).to be_nil
+    end
 
+    if Wedge.server?
       it 'should return javascript and resource maps', :slow do
         expect(Wedge.javascript).to match /Opal/
         expect(Wedge.source_map).to match /mappings/
-      end
-    else
-      it 'should not have an assets key' do
-        expect(Wedge.config.assets_key).not_to be_nil
       end
     end
   end
